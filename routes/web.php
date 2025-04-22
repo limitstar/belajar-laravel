3999ee3e1aa15b7e4336f7b35dc37e551442775c
limitstar <mrpw112@gmail.com>
2025-04-22 16:25:51 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HelloWorldController;
 use App\Http\Controllers\HtmlController;
 use App\Http\Controllers\LatihanController;
+use App\Http\Controllers\AnggotaController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -15,3 +16,4 @@ Route::get('getlorem', [HtmlController::class, 'getLorem']);
 Route::get('biodata', [HtmlController::class, 'bioData']);
 Route::get('getform', [LatihanController::class, 'getForm']);
 Route::get('gettabel', [LatihanController::class, 'getTabel']);
+Route::resource('anggota', AnggotaController::class);
